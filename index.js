3b969e1ae06f49e5f524ffab5923a804dead3416
LevCey <vrkafa@gmail.com>
2023-11-10 23:08:35 +0300

    bootstrap css styles

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
 
 const data = [
-    {id:1, name:"iphone14", price:3000, isActive:true},
-    {id:2, name:"iphone15", price:4000, isActive:false},
-    {id:3, name:"iphone16", price:5000, isActive:true},
+    {id:1, name:"iphone14", price:3000, isActive:true, imageUrl: "1.jpg"},
+    {id:2, name:"iphone15", price:4000, isActive:true, imageUrl: "2.jpg"},
+    {id:3, name:"iphone16", price:5000, isActive:true, imageUrl: "3.jpg"}
 ];
 
 // routes
